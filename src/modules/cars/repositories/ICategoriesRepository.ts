f536010b3f5f5e03a6e56991bb47cc4537201374
juanozorio <juan_ozorio@outlook.com>
2022-03-19 21:31:40 -0300

    Refatorando a listagem de categoria

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -1,17 +1,15 @@
 import { Category } from '../model/Category';
 
-
 interface ICreateCategoryDTO {
     name: string;
     description: string;
 }
 
-
 interface ICategoriesRepository {
     findByName(name: string): Category;
     list(): Category[];
-    create({ name, description}: ICreateCategoryDTO): void;
+    create({ name, description }: ICreateCategoryDTO): void;
 
 }
 
-export {ICategoriesRepository, ICreateCategoryDTO} 
+export { ICategoriesRepository, ICreateCategoryDTO };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,17 @@
 import { Router } from 'express';
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
 })
 
-categoriesRoutes.get ("/", (request, response) => {
-    const all = categoriesRepository.list();
-    return response.json(all)
+categoriesRoutes.get("/", (request, response) => {
+    return listCategoriesController.handle(request, response);
+
 })
 
 
